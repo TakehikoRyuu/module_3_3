fa63f63d636d12932bcddac20a88553ebf8586f0
Андрей Шеремет <seheremet.and@yandex.ru>
2024-07-24 15:04:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ print_params(c = [1,2,3]) # 1 строка [1, 2, 3]
 
 values_list = (9, 'Да', True)
 print_params(*values_list) # 9 Да True
-values_dict = {'a': 11, 'b': 'Когда-то', 'c': False}
-print_params(*values_dict) # 11 Когда-то False
+values_dict = {'a': 11, 'b': 'Надо', 'c': False}
+print_params(*values_dict) # 11 Надо False
 
 values_list_2 = [54.32, 'Строка']
 print_params(*values_list_2, 42) # 54.32 Строка 42
